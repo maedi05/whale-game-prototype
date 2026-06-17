7370e95a4e6da56fa247964b7618f20b50523b0e
maedi05 <mfuhrken05@gmail.com>
2026-06-17 20:24:00 +0200

    shader experiment

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 var facing_right: bool = true
-@export var projectile_speed := 300
+@export var projectile_speed := 350
 
-
-	
-
+func _ready() -> void: #timer that makes the projectile disappear
+	await get_tree().create_timer(.9).timeout
+	queue_free() 
 func _process(delta):
 	var projectile_dir = 1.0 if facing_right else -1.0
 	position.x +=  projectile_speed * projectile_dir * delta
