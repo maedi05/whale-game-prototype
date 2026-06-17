9052bb0841da4f900d81fd870df9033498717436
Carlos Huerta <carlosi.huerta@udem.edu>
2026-06-17 12:58:09 -0600

    Outline blink

--- Scripts/water_plant.gd
+++ Scripts/water_plant.gd
@@ -1,5 +1,10 @@
 extends Area2D
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_area_entered(_area: Area2D) -> void:
 	print('water plant detected')
+	var tween = get_tree().create_tween()
+	tween.tween_method(set_shader_blinkintensity, 1.0, 0.0, 0.5)
+
+func set_shader_blinkintensity(newValue : float):
+	sprite_2d.material.set_shader_parameter("blink_intensity", newValue)
