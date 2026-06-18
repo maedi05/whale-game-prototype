94275158ccb5f7de2e36df7949cb51c5573c6085
maedi05 <mfuhrken05@gmail.com>
2026-06-18 11:27:00 +0200

    figuring out lighting
    current status: not figured out yet

--- Scripts/water_plant.gd
+++ Scripts/water_plant.gd
@@ -4,7 +4,7 @@ extends Area2D
 func _on_area_entered(_area: Area2D) -> void:
 	print('water plant detected')
 	var tween = get_tree().create_tween()
-	tween.tween_method(set_shader_blinkintensity, 1.0, 0.0, 0.5)
+	tween.tween_method(set_shader_blinkintensity, 12.0, 0.0, 0.5)
 
 func set_shader_blinkintensity(newValue : float):
-	sprite_2d.material.set_shader_parameter("blink_intensity", newValue)
+	sprite_2d.material.set_shader_parameter("thickness", newValue)
